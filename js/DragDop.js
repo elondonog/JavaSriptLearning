3b1a3da5f0ef170731d72f9befafbf4ea277618e
elondonog <elondonog@yahoo.es>
2016-05-22 11:21:48 -0500

    First Commit

---
+++ js/DragDop.js
@@ -0,0 +1,110 @@
+﻿function start() {
+    var canvas = getCanvas();
+    canvas.onmousedown = myDown;
+    canvas.onmouseup = myUp;
+    canvas.myContexts = new Array();
+    canvas.lastZIndex = 0
+}
+
+function draw() {
+    var canvas = document.getElementById('canvas');
+
+    if (canvas.getContext) {
+        
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle = "black";
+        ctx.fillRect(25, 25, 100, 100);
+        ctx.clearRect(45, 45, 60, 60);
+        ctx.strokeRect(50, 50, 50, 50);
+        ctx.zindex = canvas.lastZIndex++;
+        canvas.myContexts[canvas.myContexts.length] = ctx;
+
+
+        var ctx2 = canvas.getContext('2d');
+
+        ctx2.beginPath();
+        ctx2.moveTo(50, 50);
+        ctx2.lineTo(250, 175);
+        ctx2.lineTo(200, 125);
+        ctx2.closePath();
+        ctx2.fillStyle = "red";
+        ctx2.fill();
+        ctx2.stroke();
+        canvas.myContexts[canvas.myContexts.length] = ctx2;
+
+
+    }
+}
+/*
+function rect(x, y, w, h) {
+    ctx.beginPath();
+    ctx.rect(x, y, w, h);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function clear() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+function init() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    return setInterval(draw, 10);
+}
+
+function draw() {
+    clear();
+    ctx.fillStyle = "#FAF7F8";
+    rect(0, 0, WIDTH, HEIGHT);
+    ctx.fillStyle = "#444444";
+    rect(x - 15, y - 15, 30, 30);
+}
+
+function myMove(e) {
+    if (dragok) {
+        x = e.pageX - canvas.offsetLeft;
+        y = e.pageY - canvas.offsetTop;
+    }
+}
+*/
+function myDown(e) {
+    var x = 0;
+    e.x;
+    e.y;
+
+    var canvas = getCanvas();
+    canvas.startMove = new Array();
+    canvas.startMove[0] = e.x;
+    canvas.startMove[1] = e.y;
+
+    var context = getSelectedContexts(e.x, e.y);
+
+}
+
+function myUp(e) {
+    var canvas = getCanvas();
+    canvas.startMove;
+    e.x;
+    e.y;
+    dragok = false;
+    canvas.onmousemove = null;
+
+    context.translate();
+}
+
+function getSelectedContexts(x, y) {
+    //var selectedContexts = new Array();
+    var canvas = getCanvas();
+    for (var i = 0; i < canvas.myContexts; i++) {
+        var context = canvas.myContexts[i];
+        if (coninciden) {
+            return  context
+        }
+    }
+    
+}
+
+function getCanvas(){
+    return document.getElementById('canvas');
+}
---
+++ js/default.js
@@ -0,0 +1,110 @@
+﻿function calcNumbers() {
+    //numerator
+    var x = document.getElementById("numAtor").value;
+	//validateNumber(x);
+    //denominator and number of times each circle is splited
+    var numArcs = document.getElementById("denAtor").value;
+	//validateNumber(validateNumber);
+	//defines the a variable radious
+	//var radiousValue = document.getElementById("radious").value;
+	
+    //number of cicles to draw
+    var NumCircles = Math.floor(x / numArcs);
+    //last two arcs to draw
+    var module = x - (NumCircles * numArcs);
+
+    //calc mixed fraction
+    var mixFra = document.getElementById("mixedFraction");
+    mixFra.innerHTML = "Mixed Fraction = "
+    mixFra.innerHTML += NumCircles + " " + module + "/" + numArcs;
+
+    //calc decimal 
+    var decNumber = document.getElementById("decNumber");
+    decNumber.innerHTML = "Decimal = "
+    decNumber.innerHTML += (x / numArcs).toFixed(4);
+
+	//set canvas size
+	setCanvasSize();
+	
+    //draws circle
+    drawFractionAsCircles(x, numArcs)
+}
+function validateNumber(number){
+	if(isNaN(number)){
+	alert(number + " is not a number");
+	number=null;
+ }
+ //else{
+	//document.write(number + " is a number <br/>");
+ //}
+}
+function setCanvasSize(){
+	var theCanvas = document.getElementById("myCircleCanvas");
+	
+	theCanvas.width = window.innerWidth;
+	theCanvas.height= window.innerHeight;
+		
+}
+
+function drawFractionAsCircles(numerator, denominator) {
+
+    //number of cicles to draw
+    var NumCircles = Math.floor(numerator / denominator);
+    //last two arcs to draw
+    var module = numerator - (NumCircles * denominator);
+	// angle of arcs.
+	var phase = 2 * Math.PI / denominator
+	// radious of circle.
+    var radious = parseInt(document.getElementById("radious").value);
+	//validateNumber(radious);
+    var centerX = radious + 20;
+    var centerY = radious + 20;
+    var theCanvas = document.getElementById("myCircleCanvas");
+    var canvasContext = theCanvas.getContext("2d");
+	canvasContext.clearRect(0, 0, theCanvas.width, theCanvas.height);
+    var arcColor = new Array("red", "black", "pink", "blue", "Brown", "CadetBlue", "Chocolate", "BlanchedAlmond", "DarkGray", "DarkMagenta", "blue");
+    var errorMessage = document.getElementById("Error");
+
+    for (nC = 0; nC < NumCircles; nC++) {
+        
+        for (i = 0; i < denominator; i++) {
+            
+            var startAngle = 0 + i * phase
+            var endAngle = startAngle + phase
+
+            canvasContext.beginPath();
+            canvasContext.fillStyle = arcColor[Math.floor(Math.random()*10)];
+            canvasContext.arc(centerX, centerY, radious, startAngle, endAngle);
+			canvasContext.lineTo(centerX,centerY);
+            canvasContext.stroke();
+            canvasContext.closePath();
+            canvasContext.fill();
+            //errorMessage.innerHTML = i + " Arc";
+        }
+
+        centerX = centerX + 2 * radious + 20;
+        if (centerX + radious > theCanvas.width) {
+            centerX = radious + 20;
+            centerY = centerY + 2 * radious + 20;
+        }
+    }
+	
+	if (module>0){
+	
+		for (i = 0; i < module; i++) {
+            
+            var startAngle = 0 + i * phase
+            var endAngle = startAngle + phase
+
+            canvasContext.beginPath();
+            canvasContext.fillStyle = arcColor[Math.floor(Math.random()*10)];
+            canvasContext.arc(centerX, centerY, radious, startAngle, endAngle);
+			canvasContext.lineTo(centerX,centerY);
+            canvasContext.stroke();
+            canvasContext.closePath();
+            canvasContext.fill();
+            //errorMessage.innerHTML = i + " Arc";
+        }
+	}
+}
+
